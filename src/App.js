3b63f0d84682a4f6a979f6cb3d0403d39e90323b
MarkoVolic <markovolic16@gmail.com>
2023-08-20 13:02:30 -0400

    started working on the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 function App() {
   return (
-    <div>
-      <h2></h2>
-    </div>
+     <div>
+      <center>
+        <h1 class="font">Weather</h1>
+          <div class="search">
+            <input type="text" id="myText" placeholder="Search.." />
+            <input type="submit" onclick="loadData()" class="button" value="Submit"/> 
+          </div>
+          <h2 class="location"></h2>
+            <table>
+                <tr>
+                  <th> <img src="sunny.png" class="slika" alt="img "/></th>
+                    <th><h2 class="temp"></h2></th>
+                </tr>
+            </table>
+            <h2 class="humidity"></h2>
+            <h2 class="pressure"></h2>
+            <h2 class="wind"> </h2>
+      </center>
+     </div>
   );
 }
 
