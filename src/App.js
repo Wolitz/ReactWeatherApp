9fe0a13c1dda747f2a605af6f3a8526868e0246b
MarkoVolic <markovolic16@gmail.com>
2023-08-21 12:26:33 -0400

    everything is done but images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+
+ var APIdata 
+const [data,setData ] = useState({} );
+const [location,setLocation ] = useState("" );
+
+const apiKey = "ffcc3b1d6839acddfd18b10b824cdae1";
+const apiUrl=  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+ const checkWeather = async () => {
+ const response = await fetch(apiUrl + location +'&appid=' + apiKey);
+  APIdata = await response.json();
+ setData(APIdata)
+ console.log(APIdata);
+  
+}
+
+ 
+const getData = (event) => {
+  if(event.key === 'Enter'){
+
+    checkWeather();
+  }
+}
+
   return (
      <div>
       <center>
-        <h1 class="font">Weather</h1>
-          <div class="search">
-            <input type="text" id="myText" placeholder="Search.." />
-            <input type="submit" onclick="loadData()" class="button" value="Submit"/> 
+        <h1 className="font">Weather</h1>
+          <div className="search">
+            <input 
+            type="text"
+            id="myText"
+            onChange={event => setLocation(event.target.value) }
+            onKeyUp={getData}
+            placeholder="Search..." 
+            />
           </div>
-          <h2 class="location"></h2>
+          <br/>
+          <h2 className="location">{data.name}</h2>
             <table>
                 <tr>
-                  <th> <img src="sunny.png" class="slika" alt="img "/></th>
-                    <th><h2 class="temp"></h2></th>
+                  <th> <img src="assets/sunny.png" className="slika" /></th>
+                    <th>{data.main ? <h2>{data.main.temp }°C</h2> : null }</th>
                 </tr>
             </table>
-            <h2 class="humidity"></h2>
-            <h2 class="pressure"></h2>
-            <h2 class="wind"> </h2>
+            {data.main ? <h2> H: {data.main.humidity }%</h2> : null }
+            {data.main ? <h2> P: {data.main.pressure }Ph</h2> : null } 
+            {data.wind ? <h2> S: {data.wind.speed }KM/H</h2> : null }
       </center>
      </div>
   );
